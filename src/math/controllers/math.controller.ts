489fef5f53899cb57763c415406f78f7fbb8c8fd
Connie Leung <cleung334@gmail.com>
2023-12-18 01:14:46 +0800

    feat: retrieve return value of binary and unary jobs

--- src/math/controllers/math.controller.ts
+++ src/math/controllers/math.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, BadRequestException } from '@nestjs/common';
 import { Queue } from 'bullmq';
 import { InjectMathBinaryQueue, InjectMathUnaryQueue } from '../decorators/inject-queue.decorator';
 import { BinaryOperationDto } from '../dtos/binary-operation.dto';
@@ -53,5 +53,25 @@ export class MathController {
   async negate(@Body() dto: UnaryOperationDto): Promise<string | undefined> {
     const job = await this.mathUnaryQueue.add(MATH_UNARY_OPS.NEGATE, dto);
     return job.id || '';
+  }
+
+  @Get('binary/:id/result')
+  async getBinaryJobResult(@Param('id') jobId: string): Promise<any> {
+    const binaryJob = await this.mathBinaryQueue.getJob(jobId);
+    if (binaryJob) {
+      return binaryJob.returnvalue;
+    }
+
+    throw new BadRequestException(`Binary job id: ${jobId} is not found`);
+  }
+
+  @Get('unary/:id/result')
+  async getUnaryJobResult(@Param('id') jobId: string): Promise<any> {
+    const unaryJob = await this.mathUnaryQueue.getJob(jobId);
+    if (unaryJob) {
+      return unaryJob.returnvalue;
+    }
+
+    throw new BadRequestException(`Unary job id: ${jobId} is not found`);
   }
 }
