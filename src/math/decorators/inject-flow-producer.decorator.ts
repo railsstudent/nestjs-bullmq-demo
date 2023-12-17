54c293a2c367004e224db9fe3f4976c027198de9
Connie Leung <cleung334@gmail.com>
2023-12-17 21:40:06 +0800

    refactor: inject decorators are arrow functions

--- src/math/decorators/inject-flow-producer.decorator.ts
+++ src/math/decorators/inject-flow-producer.decorator.ts
@@ -1,6 +1,4 @@
 import { InjectFlowProducer } from '@nestjs/bullmq';
 import { MATH_ARRAY_PRODUCER } from '../constants/math-array.constant';
 
-export function InjectMathComparisonProducer() {
-  return InjectFlowProducer(MATH_ARRAY_PRODUCER);
-}
+export const InjectMathComparisonProducer = () => InjectFlowProducer(MATH_ARRAY_PRODUCER);
--- src/math/decorators/inject-queue.decorator.ts
+++ src/math/decorators/inject-queue.decorator.ts
@@ -1,10 +1,5 @@
 import { InjectQueue } from '@nestjs/bullmq';
 import { MATH_BINARY, MATH_UNARY } from '../constants/math.constant';
 
-export function InjectMathBinaryQueue() {
-  return InjectQueue(MATH_BINARY);
-}
-
-export function InjectMathUnaryQueue() {
-  return InjectQueue(MATH_UNARY);
-}
+export const InjectMathBinaryQueue = () => InjectQueue(MATH_BINARY);
+export const InjectMathUnaryQueue = () => InjectQueue(MATH_UNARY);
